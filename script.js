90290d350cf4963eab402fcf83ff9b7c6b53ca7a
YvesConcepcion <118972229+YvesConcepcion@users.noreply.github.com>
2023-06-09 21:41:45 +0800

    Add switch turn function

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 "use strict";
 
-let gameBoard = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-
-//Elements
-const board = document.querySelectorAll(".board");
-console.log(board); //nodelist
+// Retrieve elements
+const boards = document.querySelectorAll(".board");
 
 const createPlayer = (name, symbol) => {
   return {
     name: name,
     symbol: symbol,
     score: 0,
-    play: function () {
-      gameBoard[0][0] = symbol;
-    },
   };
 };
-const player1 = createPlayer("Player 1", "X");
-const player2 = createPlayer("Player2", "O");
-console.log(gameBoard);
 
 const game = (() => {
+  let gameBoard = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+  const player1 = createPlayer("Player 1", "X");
+  const player2 = createPlayer("Player2", "O");
   let activePlayer = player1;
+  const switchPlayerturn = () =>
+    (activePlayer = activePlayer === player1 ? player2 : player1);
+  const playRound = boards.forEach(function (board) {
+    board.addEventListener("click", function (e) {
+      console.log();
+      board.textContent = activePlayer.symbol;
+      switchPlayerturn();
+    });
+  });
 })();
 
+// play: function () {
+//   board.addEventListener("click", function (e) {
+//     const clickedElement = event.target
+//     gameBoard[0][0] = symbol;
+//   });
 /*
 Your main goal here is to have as little global code as possible. Try tucking everything away inside of a module or factory. Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
 
