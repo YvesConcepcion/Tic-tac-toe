0c66349e2155ef0d7c730599048453135c8a9e1d
YvesConcepcion <118972229+YvesConcepcion@users.noreply.github.com>
2023-06-15 02:22:48 +0800

    Add tie conditions to game logic

--- script.js
+++ script.js
@@ -24,8 +24,9 @@ const init = function () {
   const container = document.getElementById("grid");
   for (let i = 0; i < gameBoard.length; i++) {
     const div = document.createElement("div");
-    div.classList.add("board");
+    div.classList.add("board", "active");
     div.setAttribute("data-index", [i]);
+    div.setAttribute("data-state", activePlayer.symbol);
     container.appendChild(div);
   }
   return;
@@ -33,12 +34,11 @@ const init = function () {
 
 const player1 = createPlayer("Player 1", "X");
 const player2 = createPlayer("Player2", "O");
+let activePlayer = player1;
 
 const game = (() => {
   let playing = true;
-  //Create gameBoard
   init();
-  let activePlayer = player1;
   const boards = document.querySelectorAll(".board");
 
   const switchPlayerturn = () =>
@@ -55,8 +55,12 @@ const game = (() => {
     [0, 4, 8],
     [2, 4, 6],
   ];
-
   function checkWinner() {
+    let isTie = gameBoard.every((board) => board !== "");
+    if (isTie) {
+      console.log(`TIE`);
+      return;
+    }
     for (let i = 0; i < winConditions.length; i++) {
       const condition = winConditions[i];
       const cellA = gameBoard[condition[0]];
@@ -76,10 +80,12 @@ const game = (() => {
   }
 
   const playRound = boards.forEach(function (board, i) {
+    const turn = document.querySelector("player");
     board.addEventListener("click", function () {
-      if (gameBoard[i] != "") return;
+      if (gameBoard[i] != "" || winner == true) return;
       gameBoard[i] = activePlayer.symbol;
       board.setAttribute("data-symbol", activePlayer.symbol);
+      board.classList.remove("active");
       checkWinner();
       switchPlayerturn();
     });
